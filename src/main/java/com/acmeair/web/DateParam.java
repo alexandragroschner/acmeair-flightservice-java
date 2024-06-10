c80a306c6bb6231edbd0341dd32b7b1eee781be5
Alexandra Groschner <alexandra.groschner@craftworks.at>
2024-06-10 08:23:12 +0200

    wip - fixing date parsing

--- src/main/java/com/acmeair/web/DateParam.java
+++ src/main/java/com/acmeair/web/DateParam.java
@@ -18,7 +18,10 @@ package com.acmeair.web;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.logging.Logger;
+
 import jakarta.ws.rs.WebApplicationException;
 
 // Payara and Helidon don't seem to support Date on a JAX-RS Param,
@@ -26,16 +29,31 @@ import jakarta.ws.rs.WebApplicationException;
 
 public class DateParam {
   private static final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEE MMM dd yyyy");
+  private static int year = Calendar.getInstance().get(Calendar.YEAR);
   private static final int ZERO = 48;
+  private static final Logger logger = Logger.getLogger(DateParam.class.getName());
   private Date date;
   
   public DateParam( String dateTime ) throws WebApplicationException {
-    
+
+      logger.warning("DATE TIME received: " + dateTime);
+      /*
       String dateOnly;
 
       if (dateTime.charAt(11) == ZERO) {
-        // Assume format is EEE MMM dd 00:00:00 z yyyy from jmeter. Chop off the time + timezone.
-        dateOnly = dateTime.substring(0,10) + " " + dateTime.substring(24,28);
+          logger.warning("is in 11 if");
+          // TODO: never goes into this if - fix it
+          if (dateTime.charAt(24) == 'S') {
+              logger.warning("is in 24 if");
+              // Assume format is EEE MMM dd 00:00:00 CEST yyyy from jmeter. Chop off the time + timezone.
+              dateOnly = dateTime.substring(0,10) + " " + dateTime.substring(24,29);
+              logger.warning("substring: " + dateOnly);
+          } else {
+              logger.warning("is in 11 if ELSE");
+              // Assume format is EEE MMM dd 00:00:00 z yyyy from jmeter. Chop off the time + timezone.
+              dateOnly = dateTime.substring(0,10) + " " + dateTime.substring(24,28);
+              logger.warning("substring: " + dateOnly);
+          }
       }
       else {
         // Assume format is EEE MMM dd yyyy from the browser.
@@ -44,6 +62,16 @@ public class DateParam {
 
       LocalDate localDate = LocalDate.parse(dateOnly, formatter);
       date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+      */
+
+      // new solution:
+      // A bit of hack to get the date in the correct format
+      String dateOnly = dateTime.substring(0,10) + " " + year;
+
+      LocalDate localDate = LocalDate.parse(dateOnly, formatter);
+      date = Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant());
+
+
   }
 
   public Date getDate() {
